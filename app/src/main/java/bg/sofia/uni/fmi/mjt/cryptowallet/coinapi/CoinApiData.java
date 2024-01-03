26abcfbbee5207fb1eca5d6930d7fa0d3c0c7f51
Nikolay Kostandiev <94651604+nnniki@users.noreply.github.com>
2024-01-03 22:13:11 +0200

    Update CoinApiData.java

--- app/src/main/java/bg/sofia/uni/fmi/mjt/cryptowallet/coinapi/CoinApiData.java
+++ app/src/main/java/bg/sofia/uni/fmi/mjt/cryptowallet/coinapi/CoinApiData.java
@@ -41,7 +41,7 @@ public class CoinApiData {
 
         try {
             URI uri = new URI(API_ENDPOINT_SCHEME, API_ENDPOINT_HOST, API_ENDPOINT_PATH, null);
-            Dotenv dotenv = Dotenv.configure().directory("./app").load();
+            Dotenv dotenv = Dotenv.load();
             String apiKey = dotenv.get("API_KEY");
 
             HttpRequest request = HttpRequest.newBuilder()
